08ec0fc30dff7042620c40a2bf97b37895b8ca86
natalia.belzarova <natalia.belzarova@gmail.com>
2023-08-29 18:12:23 +0200

    project structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1>Hola chicas!</h1>
+  )
+}
+
+export default App
---
+++ src/config/Router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
