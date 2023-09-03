ae753ca72fcd10a8844e65e8d6eda4135597b5ec
Jimena <jimenacastellanos81@gmail.com>
2023-09-03 12:36:26 +0200

    AnimalCard created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import AnimalCard from './components/atoms/animalCard/AnimalCard';
+import Gatete from './assets/images/gatete.jpg';
+import './components/atoms/animalCard/animalCard.css'
 
 function App() {
 
   return (
+    <>
     <h1>Hola chicas!</h1>
+
+   <div className='ac-ctss'>
+    <AnimalCard image={Gatete} title="Cleo"  description="Es una gata juguetona y cariñosa, le encanta que le rasquen la barriga." />
+    <AnimalCard image={Gatete} title="Cleo"  description="Es una gata juguetona y cariñosa, le encanta que le rasquen la barriga." /> 
+    <AnimalCard image={Gatete} title="Cleo"  description="Es una gata juguetona y cariñosa, le encanta que le rasquen la barriga." />
+    </div>
+    </>
   )
 }
 
---
+++ src/components/atoms/animalCard/AnimalCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './animalCard.css';
+
+function AnimalCard(props) {
+    return (
+      <div className="aC-cardContainer">
+        <img class='aC-img'src={props.image} alt={props.title} />
+        <div className="ac-cardText">
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+        </div>
+      </div>
+    );
+  }
+  
+
+export default AnimalCard
